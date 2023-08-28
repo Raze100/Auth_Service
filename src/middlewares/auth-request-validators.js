b3b1fc60dc9fa32739cf9ad66d4705f0197d178e
Raze100 <rawatrohit084@gmail.com>
2023-08-28 11:40:33 +0530

    added email and password validator in the incoming request

---
+++ src/middlewares/auth-request-validators.js
@@ -0,0 +1,18 @@
+//same middleware can be used for signIn and signUp so we made it common as "validateUserAuth"
+
+const validateUserAuth = (req, res, next) => {
+    if( !req.body.email || !req.body.password )
+    {
+        //if any of the above params is missing we come inside the if block
+        return res.status(400).json({
+            success: false,
+            data: {},
+            message: "Something went wrong",
+            err: "Email or Password Missing in the request"
+        })
+    }
+
+    next();
+}
+
+module.exports ={validateUserAuth} ;
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 
+const {AuthRequestValidators} = require('../../middlewares/index')
+
 const UserController = require('../../controllers/user-controller');
 
 const router = express.Router();
 
-router.post('/signup' , UserController.create);
-router.post('/signin', UserController.signIn);
+router.post('/signup', AuthRequestValidators.validateUserAuth, UserController.create);
+router.post('/signin', AuthRequestValidators.validateUserAuth, UserController.signIn);
 router.delete('/signup/:id', UserController.destroy);
-router.get('/signup/:id', UserController.getById);
+router.get('/signup/:id', AuthRequestValidators.validateUserAuth, UserController.getById);
 
 module.exports = router; 
